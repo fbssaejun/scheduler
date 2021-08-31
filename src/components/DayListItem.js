8832208684c955e1dacc258705582b1db7c49694
Anthony Kim <fbssaejun@gmail.com>
2021-08-31 15:33:29 -0400

    add function to show days conditionally

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -8,11 +8,21 @@ export default function DayListItem(props) {
     "day-list__item--selected" : props.selected,
     "day-list__item--full" : !props.spots
   });
+
+  const formatSpots = (spots) => {
+    if(spots ===  1) {
+      return '1 spot '
+    } else if (spots > 1) {
+      return spots + ' spots'
+    } else {
+      return 'no spots'
+    }
+  }
   
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)} >
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots(props.spots)} remaining</h3>
     </li>
   );
 }
