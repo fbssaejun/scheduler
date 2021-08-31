ed6594b4806a8c5ce24d053253f8b33cfa9b45ce
Anthony Kim <fbssaejun@gmail.com>
2021-08-31 16:35:16 -0400

    use state for days

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import "components/Application.scss";
 import DayList from "./DayList";
 
@@ -21,13 +21,16 @@ const days = [
 ];
 
 export default function Application(props) {
+
+  const [currentDay, setCurrentDay] = useState("")
+  
   return (
 
     <main className="layout">
       <section className="sidebar">
       <img className="sidebar--centered" src="images/logo.png" alt="Interview Scheduler" />
       <hr className="sidebar__separator sidebar--centered" />
-      <nav className="sidebar__menu"><DayList days={days} day={"Monday"} setDay={day => console.log(day)}/></nav>
+      <nav className="sidebar__menu"><DayList days={days} day={currentDay} setDay={setCurrentDay}/></nav>
       <img className="sidebar__lhl sidebar--centered" src="images/lhl.png" alt="Lighthouse Labs" />
       </section>
       <section className="schedule">
