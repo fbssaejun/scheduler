86fe7592e052545294a3522805ccbc47f1eb5791
Anthony Kim <fbssaejun@gmail.com>
2021-09-03 18:18:54 -0400

    use hook to show conditionally

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,17 +1,32 @@
-import React from 'react'
-import "./styles.scss"
-import Header from './Header'
-import Show from './Show'
-import Empty from './Empty'
+import React from "react";
+import "./styles.scss";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
+import useVisualMode from "hooks/useVisualMode";
+import Form from "./Form";
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 export default function Appointment(props) {
-
-  const {time, interview} = props
+  const { time, interview, interviewers=[] } = props;
+  const { mode, transition, back } = useVisualMode(
+    interview ? SHOW : EMPTY
+  );
 
   return (
     <article className="apoointment">
       <Header time={time} />
-      {interview ? <Show student={interview.student} interviewer={interview.interviewer} /> : <Empty/>}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === CREATE && <Form interviewers={interviewers} onCancel={back} />}
+      {mode === SHOW && (
+        <Show
+        student={interview.student}
+        interviewer={interview.interviewer}
+        />
+        )}
     </article>
-  )
+  );
 }
