3c3474fb591ac0e834f5c74a6680daa5112ca793
Anthony Kim <fbssaejun@gmail.com>
2021-09-03 12:25:38 -0400

    add get interview selector

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,12 +1,11 @@
 export function getAppointmentsForDay(state, day) {
-  const findDayName = state.days.find(d => d.name === day);
+  const findDayName = state.days.find(dayList => dayList.name === day);
 
-  if(findDayName === undefined) {
-    return [];
-  }
-  
-  return findDayName.appointments.map(appointment => {
-     return state.appointments[appointment]
-  })
-
+  return !findDayName ? [] : findDayName.appointments.map(appointment => {
+    return state.appointments[appointment]
+ })
 };
+
+export function getInterview(state, interview) { 
+  return !interview ? null :  {...interview,  "interviewer" : state.interviewers[interview.interviewer]};
+}
