2797f2ee13193cce5da6be7c8a20f973e4c6cc48
Anthony Kim <fbssaejun@gmail.com>
2021-09-03 19:22:26 -0400

    add new selector function and test cases

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -9,3 +9,10 @@ export function getAppointmentsForDay(state, day) {
 export function getInterview(state, interview) { 
   return !interview ? null :  {...interview,  "interviewer" : state.interviewers[interview.interviewer]};
 }
+
+export function getInterviewersForDay(state, day) {
+  const findDayName = state.days.find(dayList => dayList.name === day);
+  return !findDayName ? [] : findDayName.interviewers.map(interviewer => {
+    return state.interviewers[interviewer]
+  })
+};
