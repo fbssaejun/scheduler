46883a983da7cdd77187e022908cc44286106988
Anthony Kim <fbssaejun@gmail.com>
2021-09-07 17:26:55 -0400

    add validation for text input and show error message

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -7,6 +7,7 @@ export default function Form(props) {
   const { interviewers, onSave, onCancel } = props;
   const [name, setName] = useState(props.name || "")
   const [interviewer, setInterviewer] = useState(props.interviewer || null)
+  const [error, setError] = useState("")
 
   const reset = () => {
     setName("")
@@ -18,6 +19,13 @@ export default function Form(props) {
     onCancel()
   }
 
+  const validate = () => {
+    if(name === "") {
+      return setError("Student name cannot be blank"); 
+    }
+    onSave(name, interviewer)
+  }
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -27,10 +35,12 @@ export default function Form(props) {
             name={name}
             type="text"
             value={name}
-            placeholder="Enter student name"
+            placeholder="Enter Student Name"
             onChange = {event => {setName(event.target.value)}}
+            data-testid="student-name-input"
           />
         </form>
+        <section className="appointment__validation">{error}</section>
         <InterviewerList
           interviewers={interviewers}
           value={interviewer}
@@ -40,7 +50,7 @@ export default function Form(props) {
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button onClick={cancel} danger>Cancel</Button>
-          <Button onClick={() => onSave(name, interviewer)} confirm>Save</Button>
+          <Button onClick={validate} confirm>Save</Button>
         </section>
       </section>
     </main>
